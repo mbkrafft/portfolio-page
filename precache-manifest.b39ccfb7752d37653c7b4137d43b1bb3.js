17534d3c7ebf8536a53550445f21b3b96893cfdf
Mathias Krafft <mbkrafft@gmail.com>
2019-10-30 15:03:26 +0100

    Updates

---
+++ precache-manifest.b39ccfb7752d37653c7b4137d43b1bb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd3f753ba478a05e83afb3a5367bf25e",
+    "url": "/portfolio-page/index.html"
+  },
+  {
+    "revision": "65e4c7b2979b54ccb86b",
+    "url": "/portfolio-page/static/css/main.e8c24495.chunk.css"
+  },
+  {
+    "revision": "62c19817db0040391150",
+    "url": "/portfolio-page/static/js/2.e306d80b.chunk.js"
+  },
+  {
+    "revision": "65e4c7b2979b54ccb86b",
+    "url": "/portfolio-page/static/js/main.40421af1.chunk.js"
+  },
+  {
+    "revision": "c184a9e998e1f4999536",
+    "url": "/portfolio-page/static/js/runtime-main.43ece5aa.js"
+  }
+]);
